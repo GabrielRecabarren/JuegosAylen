23aed3d66be61578d3bda48ccbea5fd79d7cd39f
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-09-13 03:57:20 -0300

    First Colors and Button

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/Button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Botones</h1>
+      <div className="container">
+        <Button
+        btnName={"Rot-13"}
+        btnNum={"1"}
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
